f12125d6b2ab34712b2b7e60c5f74cec0b9ae28f
Gaston Apioli <gaston.apioli@telefonica.com>
2023-05-27 15:09:54 -0300

    update 27/05

---
+++ database/migrations/2023_05_27_140706_create_cart_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_product', function (Blueprint $table) {
+            $table->bigInteger('cart_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->integer('quantity')->unsigned();
+
+            $table->foreign('cart_id')->references('id')->on('cart');
+            $table->foreign('product_id')->references('id')->on('product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_product');
+    }
+};
