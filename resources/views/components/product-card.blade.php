b442a93884806f3e133ebd0a5fe3b4d0cf710dea
Gastón Apioli <gaston.apioli@telefonica.com>
2023-06-24 18:14:53 -0300

    update

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -9,8 +9,7 @@
 <div class="card-body">
         <h4 class="text-right">${{ $product->price }}</h4>
         <h6 class="card-subtitle mb-2 text-muted">{{ $product->description }}</h6>
-        <p class="card-text">
-           
+        <p class="card-text">           
             <p>{{ $product->stock }}</p>
             <p>{{ $product->status }}</p>
         </p>
